f7efbf2eef7107c8c321ed68bc3da9d8ce84d01c
Naiteek Sangani <42254991+naiteeks@users.noreply.github.com>
2021-02-17 16:31:02 -0800

    Add script to prep device for eflow

---
+++ edge/setup/prep_device.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+#######################################################################################################################
+# This script creates folders and downlaods the media samples needed for running LVA
+#######################################################################################################################
+
+sudo mkdir /home/lvaadmin
+sudo chown -R iotedge-user:users /home/lvaadmin/
+
+mkdir /home/lvaadmin/samples
+mkdir /home/lvaadmin/samples/input
+mkdir /home/lvaadmin/samples/output
+
+curl https://lvamedia.blob.core.windows.net/public/camera-300s.mkv > /home/lvaadmin/samples/input/camera-300s.mkv
+curl https://lvamedia.blob.core.windows.net/public/lots_284.mkv > /home/lvaadmin/samples/input/lots_284.mkv
+curl https://lvamedia.blob.core.windows.net/public/lots_015.mkv > /home/lvaadmin/samples/input/lots_015.mkv
+curl https://lvamedia.blob.core.windows.net/public/t2.mkv > /home/lvaadmin/samples/input/t2.mkv
+
+sudo mkdir /var/local/mediaservices
+sudo mkdir /var/local/mediaservices/tmp
+sudo mkdir /var/lib/azuremediaservices
+sudo mkdir /var/media
+
+sudo chown -R iotedge-user:users /var/local/mediaservices/
+sudo chown -R iotedge-user:users /var/lib/azuremediaservices/
+sudo chown -R iotedge-user:users /var/media/
