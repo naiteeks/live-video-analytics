4bc29f50205b1377e9c09a7eaad20e4eddbe4fb3
Naiteek Sangani <42254991+naiteeks@users.noreply.github.com>
2021-02-17 23:47:32 -0800

    Add new user for LVA

--- edge/setup/prep_device.sh
+++ edge/setup/prep_device.sh
@@ -1,29 +1,31 @@
 #!/usr/bin/env bash
 
-#######################################################################################################################
-# This script creates folders and downloads the media samples needed for running LVA
-#######################################################################################################################
+##################################################################################################
+
+# This script creates folders and downloads the media samples needed for running LVA             #
+
+##################################################################################################
 
 sudo groupadd -g 1010 localuser
-sudo adduser --home /home/edgeuser --uid 1010 -gid 1010 edgeuser
+sudo useradd --home-dir /home/edgeuser --uid 1010 --gid 1010 edgeuser
+sudo mkdir -p /home/lvaadmin
 
-sudo mkdir /home/lvaadmin
-sudo chown -R edgeuser:localuser /home/lvaadmin/
+sudo mkdir -p /home/lvaadmin/samples
+sudo mkdir -p /home/lvaadmin/samples/input
+sudo mkdir -p /home/lvaadmin/samples/output
 
-mkdir /home/lvaadmin/samples
-mkdir /home/lvaadmin/samples/input
-mkdir /home/lvaadmin/samples/output
+sudo curl https://lvamedia.blob.core.windows.net/public/camera-300s.mkv --output /home/lvaadmin/samples/input/camera-300s.mkv
+sudo curl https://lvamedia.blob.core.windows.net/public/lots_284.mkv --output /home/lvaadmin/samples/input/lots_284.mkv
+sudo curl https://lvamedia.blob.core.windows.net/public/lots_015.mkv --output /home/lvaadmin/samples/input/lots_015.mkv
+sudo curl https://lvamedia.blob.core.windows.net/public/t2.mkv --output /home/lvaadmin/samples/input/t2.mkv
 
-curl https://lvamedia.blob.core.windows.net/public/camera-300s.mkv > /home/lvaadmin/samples/input/camera-300s.mkv
-curl https://lvamedia.blob.core.windows.net/public/lots_284.mkv > /home/lvaadmin/samples/input/lots_284.mkv
-curl https://lvamedia.blob.core.windows.net/public/lots_015.mkv > /home/lvaadmin/samples/input/lots_015.mkv
-curl https://lvamedia.blob.core.windows.net/public/t2.mkv > /home/lvaadmin/samples/input/t2.mkv
-
-sudo mkdir /var/local/mediaservices
-sudo mkdir /var/local/mediaservices/tmp
-sudo mkdir /var/lib/azuremediaservices
-sudo mkdir /var/media
+sudo mkdir -p /var/local/mediaservices
+sudo mkdir -p /var/local/mediaservices/tmp
+sudo mkdir -p /var/lib/azuremediaservices
+sudo mkdir -p /var/media
 
 sudo chown -R edgeuser:localuser /var/local/mediaservices/
 sudo chown -R edgeuser:localuser /var/lib/azuremediaservices/
 sudo chown -R edgeuser:localuser /var/media/
+
+sudo chown -R edgeuser:localuser /home/lvaadmin/
