452450052a699a6ff5549853ae6040941b0774d4
Naiteek Sangani <42254991+naiteeks@users.noreply.github.com>
2021-02-18 11:08:36 -0800

    fixes

--- edge/setup/prep_device.sh
+++ edge/setup/prep_device.sh
@@ -6,25 +6,22 @@
 
 ##################################################################################################
 
-sudo groupadd -g 1010 localuser
-sudo useradd --home-dir /home/edgeuser --uid 1010 --gid 1010 edgeuser
-sudo mkdir -p /home/lvaadmin
+sudo groupadd -g 1010 localusergroup
+sudo useradd --home-dir /home/edgeuser --uid 1010 --gid 1010 lvaedgeuser
+sudo mkdir -p /home/lvaedgeuser
 
-sudo mkdir -p /home/lvaadmin/samples
-sudo mkdir -p /home/lvaadmin/samples/input
+sudo mkdir -p /home/lvaedgeuser/samples
+sudo mkdir -p /home/lvaedgeuser/samples/input
 
-sudo curl https://lvamedia.blob.core.windows.net/public/camera-300s.mkv --output /home/lvaadmin/samples/input/camera-300s.mkv
-sudo curl https://lvamedia.blob.core.windows.net/public/lots_284.mkv --output /home/lvaadmin/samples/input/lots_284.mkv
-sudo curl https://lvamedia.blob.core.windows.net/public/lots_015.mkv --output /home/lvaadmin/samples/input/lots_015.mkv
-sudo curl https://lvamedia.blob.core.windows.net/public/t2.mkv --output /home/lvaadmin/samples/input/t2.mkv
+sudo curl https://lvamedia.blob.core.windows.net/public/camera-300s.mkv --output /home/lvaedgeuser/samples/input/camera-300s.mkv
+sudo curl https://lvamedia.blob.core.windows.net/public/lots_284.mkv --output /home/lvaedgeuser/samples/input/lots_284.mkv
+sudo curl https://lvamedia.blob.core.windows.net/public/lots_015.mkv --output /home/lvaedgeuser/samples/input/lots_015.mkv
+sudo curl https://lvamedia.blob.core.windows.net/public/t2.mkv --output /home/lvaedgeuser/samples/input/t2.mkv
 
-sudo mkdir -p /var/local/mediaservices
-sudo mkdir -p /var/local/mediaservices/tmp
 sudo mkdir -p /var/lib/azuremediaservices
 sudo mkdir -p /var/media
 
-sudo chown -R edgeuser:localuser /var/local/mediaservices/
-sudo chown -R edgeuser:localuser /var/lib/azuremediaservices/
-sudo chown -R edgeuser:localuser /var/media/
+sudo chown -R lvaedgeuser:localusergroup /var/lib/azuremediaservices/
+sudo chown -R lvaedgeuser:localusergroup /var/media/
 
-sudo chown -R edgeuser:localuser /home/lvaadmin/
+sudo chown -R lvaedgeuser:localusergroup /home/lvaedgeuser/
